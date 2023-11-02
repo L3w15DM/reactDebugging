70704bfc6a0995250bdac450a313ab3fe37d3944
L3w15DM <117673970+L3w15DM@users.noreply.github.com>
2023-11-02 17:34:50 -0500

    BookTable can access books

--- src/components/BookTable/BookTable.jsx
+++ src/components/BookTable/BookTable.jsx
@@ -6,7 +6,7 @@ import "./BookTable.css";
 // How can you give BookTable access to that books array?
 
 const BookTable = () => {
-  const bookItems = books.map((book) => <div>{book}</div>);
+  const bookItems = books.map((book) => <div key={book.id}>{book.title}</div>);
 
   return (
     <div>
